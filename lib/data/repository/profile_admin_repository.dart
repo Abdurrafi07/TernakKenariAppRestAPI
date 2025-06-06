85463aa9fd06c9309c6b4e0d154d97c0feacfa74
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-06-06 13:29:37 +0700

    Membuat class ProfileAdminRepository

---
+++ lib/data/repository/profile_admin_repository.dart
@@ -0,0 +1,9 @@
+import 'package:canary_template/services/service_http_client.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class ProfileAdminRepository {
+  final ServiceHttpClient _serviceHttpClient;
+  final secureStorage = FlutterSecureStorage();
+
+  ProfileAdminRepository(this._serviceHttpClient);
+}
