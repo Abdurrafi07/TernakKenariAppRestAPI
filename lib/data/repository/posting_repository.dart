c08ddb718edb80cd0527eca234c4b3d44075237d
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-06-06 15:03:59 +0700

    Menambahkan fungsi addPostBurung untuk posting data burung yang dijual

--- lib/data/repository/posting_repository.dart
+++ lib/data/repository/posting_repository.dart
@@ -1,6 +1,36 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:canary_template/data/model/request/admin/posting_jual_request_model.dart';
+import 'package:canary_template/data/model/response/burung_semua_tersedia_model.dart';
 import 'package:canary_template/services/service_http_client.dart';
+import 'package:dartz/dartz.dart';
 
 class PostingRepository {
   final ServiceHttpClient _serviceHttpClient;
   PostingRepository(this._serviceHttpClient);
+
+  Future<Either<String, BurungSemuaTersediabyIdModel>> addPostBurung(
+    PostingJualRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.post(
+        'admin/posting-jual',
+        requestModel.toMap(),
+      );
+      final jsonResponse = json.decode(response.body);
+      if (response.statusCode == 201) {
+        final profileResponse = BurungSemuaTersediabyIdModel.fromJson(
+          jsonResponse,
+        );
+        log("Add Burung successful: ${profileResponse.message}");
+        return Right(profileResponse);
+      } else {
+        log("Add Burung failed: ${jsonResponse['message']}");
+        return Left(jsonResponse['message'] ?? 'Add Burunng failed');
+      }
+    } catch (e) {
+      log("Error in Add burung: $e");
+      return Left("An error occurred while post burung: $e");
+    }
+  }
 }
