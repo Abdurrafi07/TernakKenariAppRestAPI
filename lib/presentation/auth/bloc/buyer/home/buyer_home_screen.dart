c0ebc31c542a6814c4891d57cd8ed2ec7725d31a
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-06-06 20:43:35 +0700

    Memperbaiki Typo Stateless -> Stateful

--- lib/presentation/auth/bloc/buyer/home/buyer_home_screen.dart
+++ lib/presentation/auth/bloc/buyer/home/buyer_home_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
-class BuyerHomeScreen extends StatelessWidget {
+class BuyerHomeScreen extends StatefulWidget {
   const BuyerHomeScreen({super.key});
 
   @override
+  State<BuyerHomeScreen> createState() => _BuyerHomeScreenState();
+}
+
+class _BuyerHomeScreenState extends State<BuyerHomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+    );
   }
 }
