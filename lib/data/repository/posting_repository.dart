9e2cb6d80037eb4781e895d984bd01b19a1b1216
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-06-06 14:59:39 +0700

    Membuat repository PostingRepository

---
+++ lib/data/repository/posting_repository.dart
@@ -0,0 +1,6 @@
+import 'package:canary_template/services/service_http_client.dart';
+
+class PostingRepository {
+  final ServiceHttpClient _serviceHttpClient;
+  PostingRepository(this._serviceHttpClient);
+}
