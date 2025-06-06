fa013356f291094f76c1c29e40b7a1702e6f0888
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-06-06 15:31:29 +0700

    Menambahkan fitur login dan simpan token serta role ke secure storage

--- lib/data/repository/auth_repository.dart
+++ lib/data/repository/auth_repository.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:canary_template/data/model/request/auth/login_request_model.dart';
+import 'package:canary_template/data/model/response/auth_response_model.dart';
 import 'package:canary_template/services/service_http_client.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class AuthRepository {
@@ -6,4 +11,32 @@ class AuthRepository {
   final secureStorage = FlutterSecureStorage();
 
   AuthRepository(this._serviceHttpClient);
+
+    Future<Either<String, AuthResponseModel>> login(
+    LoginRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.post(
+        'login',
+        requestModel.toMap(),
+      );
+      final jsonResponse = json.decode(response.body);
+      if (response.statusCode == 200) {
+        final loginResponse = AuthResponseModel.fromMap(jsonResponse);
+        await secureStorage.write(
+          key: 'authToken',
+          value: loginResponse.user!.token,
+        );
+        await secureStorage.write(
+          key: 'userRole',
+          value: loginResponse.user!.role,
+        );
+        return Right(loginResponse);
+      } else {
+        return Left(jsonResponse['message'] ?? 'Login failed');
+      }
+    } catch (e) {
+      return left('An error occured while logging in.');
+    }
+  }
 }
