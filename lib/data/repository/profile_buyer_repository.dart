d180c6ebdea29ed7445a29082b0357868bb4d380
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-06-06 19:18:32 +0700

    Menambahkan fungsi getProfileBuyer untuk ambil data profil

--- lib/data/repository/profile_buyer_repository.dart
+++ lib/data/repository/profile_buyer_repository.dart
@@ -9,7 +9,7 @@ class ProfileBuyerRepository {
   final ServiceHttpClient _serviceHttpClient;
   ProfileBuyerRepository(this._serviceHttpClient);
 
-    Future<Either<String, BuyerProfileResponseModel>> addProfileBuyer(
+  Future<Either<String, BuyerProfileResponseModel>> addProfileBuyer(
     BuyerProfileRequestModel requestModel,
   ) async {
     try {
@@ -30,6 +30,26 @@ class ProfileBuyerRepository {
       }
     } catch (e) {
       return Left("An error occurred while adding profile: $e");
+    }
+  }
+
+  Future<Either<String, BuyerProfileResponseModel>> getProfileBuyer() async {
+    try {
+      final response = await _serviceHttpClient.get("buyer/profile");
+
+      if (response.statusCode == 200) {
+        final jsonResponse = json.decode(response.body);
+        final profileResponse = BuyerProfileResponseModel.fromJson(
+          jsonResponse,
+        );
+        print("Profile Response: $profileResponse");
+        return Right(profileResponse);
+      } else {
+        final errorMessage = json.decode(response.body);
+        return Left(errorMessage['message'] ?? "Unknown error occurred");
+      }
+    } catch (e) {
+      return Left("An error occurred while fetching profile: $e");
     }
   }
 }
