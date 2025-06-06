1c95b40d5281fb50ff6dac9b43d7446ecf9df336
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-06-06 16:56:53 +0700

    Integrasi login menggunakan BlocConsumer dengan validasi dan respon berdasarkan role

--- lib/presentation/auth/login_screen.dart
+++ lib/presentation/auth/login_screen.dart
@@ -1,7 +1,12 @@
 import 'package:canary_template/core/components/components.dart';
-import 'package:canary_template/core/components/spaces.dart';
 import 'package:canary_template/core/constants/colors.dart';
+import 'package:canary_template/core/core.dart';
+import 'package:canary_template/data/model/request/auth/login_request_model.dart';
+import 'package:canary_template/presentation/auth/bloc/login/login_bloc.dart';
+import 'package:canary_template/presentation/auth/bloc/login/login_event.dart';
+import 'package:canary_template/presentation/auth/bloc/login/login_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -69,12 +74,67 @@ class _LoginScreenState extends State<LoginScreen> {
                     padding: const EdgeInsets.all(8.0),
                     child: Icon(Icons.lock),
                   ),
-                  suffixIcon: IconButton(onPressed: (){setState(() {
-                    isShowPassword = !isShowPassword;
-                  });}, icon: Icon(
-                    isShowPassword ? Icons.visibility : Icons.visibility_off,
-                    color: AppColors.grey,
-                  )),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isShowPassword = !isShowPassword;
+                      });
+                    },
+                    icon: Icon(
+                      isShowPassword ? Icons.visibility : Icons.visibility_off,
+                      color: AppColors.grey,
+                    ),
+                  ),
+                ),
+                const SpaceHeight(30),
+                BlocConsumer<LoginBloc, LoginState>(
+                  listener: (context, state) {
+                    if (state is LoginFailure) {
+                      ScaffoldMessenger.of(
+                        context,
+                      ).showSnackBar(SnackBar(content: Text(state.error)));
+                    } else if (state is LoginSuccess) {
+                      final role =
+                          state.responseModel.user?.role?.toLowerCase();
+                      if (role == 'admin') {
+                        // context.pushAndRemoveUntil(
+                        //   const AdminConfirmScreen(),
+                        //   (route) => false,
+                        // );
+                      } else if (role == 'buyer') {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text(state.responseModel.message!)),
+                        );
+                        // context.pushAndRemoveUntil(
+                        //   const BuyerProfilScreen(),
+                        //   (route) => false,
+                        // );
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Role tidak dikenali')),
+                        );
+                      }
+                    }
+                  },
+                  builder: (context, state) {
+                    return Button.filled(
+                      onPressed:
+                          state is LoginLoading
+                              ? null
+                              : () {
+                                if (_key.currentState!.validate()) {
+                                  final request = LoginRequestModel(
+                                    email: emailController.text,
+                                    password: passwordController.text,
+                                  );
+                                  context.read<LoginBloc>().add(
+                                    LoginRequested(requestModel: request),
+                                  );
+                                }
+                              },
+                      label: state is LoginLoading ? 'Memuat...' : 'Masuk',
+                    );
+                  },
                 ),
               ],
             ),
