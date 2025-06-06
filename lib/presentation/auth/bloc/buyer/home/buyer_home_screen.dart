d3cf904447b501849dd81af71c9a0b01b69084e3
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-06-06 21:05:22 +0700

    Menambahkan fitur logout dengan konfirmasi

--- lib/presentation/auth/bloc/buyer/home/buyer_home_screen.dart
+++ lib/presentation/auth/bloc/buyer/home/buyer_home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:canary_template/presentation/auth/login_screen.dart';
 import 'package:canary_template/presentation/bloc/get_all_burung_tersedia/get_burung_tersedia_bloc.dart';
 import 'package:canary_template/presentation/bloc/get_all_burung_tersedia/get_burung_tersedia_event.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -12,7 +14,7 @@ class BuyerHomeScreen extends StatefulWidget {
 
 class _BuyerHomeScreenState extends State<BuyerHomeScreen> {
   @override
-  void initState(){
+  void initState() {
     super.initState();
     context.read<GetBurungTersediaBloc>().add(GetAllBurungTersediaEvent());
   }
@@ -24,7 +26,44 @@ class _BuyerHomeScreenState extends State<BuyerHomeScreen> {
       appBar: AppBar(
         title: const Text("Home Screen"),
         centerTitle: true,
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.white,
+
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout, color: Colors.black),
+            //pakai loading
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return CupertinoAlertDialog(
+                    title: const Text("Konfirmasi"),
+                    content: const Text("Apakah Anda yakin ingin keluar?"),
+                    actions: [
+                      CupertinoDialogAction(
+                        child: const Text("Batal"),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      CupertinoDialogAction(
+                        child: const Text("Keluar"),
+                        onPressed: () {
+                          Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute(
+                              builder: (context) => const LoginScreen(),
+                            ),
+                            (route) => false,
+                          );
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          ),
+        ],
       ),
     );
   }
