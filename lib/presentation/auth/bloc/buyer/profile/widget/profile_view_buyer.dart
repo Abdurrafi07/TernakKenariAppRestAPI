d77ce74037d475c2176861834deae7f282fc6f73
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-06-06 19:32:38 +0700

    Mengubah ProfileViewBuyer StatefulWidget -> StatelessWidget

--- lib/presentation/auth/bloc/buyer/profile/widget/profile_view_buyer.dart
+++ lib/presentation/auth/bloc/buyer/profile/widget/profile_view_buyer.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 
-class ProfileViewBuyer extends StatefulWidget {
+class ProfileViewBuyer extends StatelessWidget {
   const ProfileViewBuyer({super.key});
 
-  @override
-  State<ProfileViewBuyer> createState() => _ProfileViewBuyerState();
-}
-
-class _ProfileViewBuyerState extends State<ProfileViewBuyer> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
