b09acec8c60df901f78fba05d22963a127be07c0
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-06-06 19:26:14 +0700

    Membuat ProfileViewBuyer StatefulWidget

---
+++ lib/presentation/auth/bloc/buyer/profile/widget/profile_view_buyer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfileViewBuyer extends StatefulWidget {
+  const ProfileViewBuyer({super.key});
+
+  @override
+  State<ProfileViewBuyer> createState() => _ProfileViewBuyerState();
+}
+
+class _ProfileViewBuyerState extends State<ProfileViewBuyer> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
