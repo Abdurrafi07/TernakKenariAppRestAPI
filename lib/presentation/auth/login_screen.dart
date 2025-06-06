c94b888ea142e05a44b57a68f60c4cc2d58b1ac1
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-06-06 18:46:41 +0700

    Menambahkan teks untuk Navigasi ke halaman register

--- lib/presentation/auth/login_screen.dart
+++ lib/presentation/auth/login_screen.dart
@@ -5,6 +5,8 @@ import 'package:canary_template/data/model/request/auth/login_request_model.dart
 import 'package:canary_template/presentation/auth/bloc/login/login_bloc.dart';
 import 'package:canary_template/presentation/auth/bloc/login/login_event.dart';
 import 'package:canary_template/presentation/auth/bloc/login/login_state.dart';
+import 'package:canary_template/presentation/auth/register_screen.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -135,6 +137,27 @@ class _LoginScreenState extends State<LoginScreen> {
                       label: state is LoginLoading ? 'Memuat...' : 'Masuk',
                     );
                   },
+                ),
+                const SpaceHeight(20),
+                Text.rich(
+                  TextSpan(
+                    text: 'Belum memiliki akun? Silahkan ',
+                    style: TextStyle(
+                      color: AppColors.grey,
+                      fontSize: MediaQuery.of(context).size.width * 0.03,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: 'Daftar disini!',
+                        style: TextStyle(color: AppColors.primary),
+                        recognizer:
+                            TapGestureRecognizer()
+                              ..onTap = () {
+                                context.push(const RegisterScreen());
+                              },
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
