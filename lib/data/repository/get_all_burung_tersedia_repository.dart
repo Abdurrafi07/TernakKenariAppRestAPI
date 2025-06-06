25ff0019c60fc01502affb76eb48e1ed7cf0e782
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-06-06 15:07:32 +0700

    Membuat repository GetAllBurungTersediaRepository

---
+++ lib/data/repository/get_all_burung_tersedia_repository.dart
@@ -0,0 +1,6 @@
+import 'package:canary_template/services/service_http_client.dart';
+
+class GetAllBurungTersediaRepository {
+    final ServiceHttpClient httpClient;
+  GetAllBurungTersediaRepository(this.httpClient);
+}
