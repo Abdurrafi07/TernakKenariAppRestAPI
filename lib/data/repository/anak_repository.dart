cf5d6db648fc15f204f21c0fd1df737bf808d6ca
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-06-06 14:49:46 +0700

    Membuat repository AnakRepository

---
+++ lib/data/repository/anak_repository.dart
@@ -0,0 +1,7 @@
+import 'package:canary_template/services/service_http_client.dart';
+
+class AnakRepository {
+  final ServiceHttpClient _serviceHttpClient;
+
+  AnakRepository(this._serviceHttpClient);
+}
