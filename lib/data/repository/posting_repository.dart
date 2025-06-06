e069b4837e89b47a7cc46fa5db71e4a2c5e5be2f
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-06-06 15:06:08 +0700

    Menambahkan fungsi getAllBurung untuk mengambil semua data burung

--- lib/data/repository/posting_repository.dart
+++ lib/data/repository/posting_repository.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:developer';
 import 'package:canary_template/data/model/request/admin/posting_jual_request_model.dart';
 import 'package:canary_template/data/model/response/burung_semua_tersedia_model.dart';
+import 'package:canary_template/data/model/response/get_all_burung_response_model.dart';
 import 'package:canary_template/services/service_http_client.dart';
 import 'package:dartz/dartz.dart';
 
@@ -26,11 +27,28 @@ class PostingRepository {
         return Right(profileResponse);
       } else {
         log("Add Burung failed: ${jsonResponse['message']}");
-        return Left(jsonResponse['message'] ?? 'Add Burunng failed');
+        return Left(jsonResponse['message'] ?? 'Add Burung failed');
       }
     } catch (e) {
       log("Error in Add burung: $e");
       return Left("An error occurred while post burung: $e");
+    }
+  }
+
+  Future<Either<String, GetAllBurungModel>> getAllBurung() async {
+    try {
+      final response = await _serviceHttpClient.get("admin/burung-semua");
+      if (response.statusCode == 200) {
+        final jsonResponse = json.decode(response.body);
+        final getAllBurung = GetAllBurungModel.fromMap(jsonResponse);
+
+        return Right(getAllBurung);
+      } else {
+        final jsonResponse = json.decode(response.body);
+        return Left(jsonResponse['message'] ?? "Get All burung failed");
+      }
+    } catch (e) {
+      return Left("An error occurred while getting all burung: $e");
     }
   }
 }
